641daa2620a09be9d99d5110da2bb4ad32e57a14
lLeoNeedl <274852@gmail.com>
2023-01-08 12:35:07 +0200

    Creating domain layer

--- app/src/main/java/com/example/shoppinglist/domain/GetShopItemUseCase.kt
+++ app/src/main/java/com/example/shoppinglist/domain/GetShopItemUseCase.kt
@@ -3,5 +3,4 @@ package com.example.shoppinglist.domain
 class GetShopItemUseCase (private val shopListRepository: ShopListRepository) {
 
     fun getShopItem(shopItemId: Int): ShopItem = shopListRepository.getShopItem(shopItemId)
-
 }
--- app/src/main/java/com/example/shoppinglist/domain/RemoveShopItemUseCase.kt
+++ app/src/main/java/com/example/shoppinglist/domain/RemoveShopItemUseCase.kt
@@ -1,6 +1,8 @@
 package com.example.shoppinglist.domain
 
-class RemoveShopItemUseCase (private val shopListRepository: ShopListRepository) {
+class RemoveShopItemUseCase(private val shopListRepository: ShopListRepository) {
 
-    fun removeShopItem(shopItem: ShopItem) = shopListRepository.removeShopItem(shopItem)
+    fun removeShopItem(shopItem: ShopItem) {
+        shopListRepository.removeShopItem(shopItem)
+    }
 }
