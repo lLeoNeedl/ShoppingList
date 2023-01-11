79e6ed82088963e1f78401c8b617c14d4ed09604
lLeoNeedl <274852@gmail.com>
2023-01-11 17:05:49 +0200

    Creating ListAdapter

--- app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
@@ -25,7 +25,7 @@ class MainActivity : AppCompatActivity() {
         setupRecyclerView()
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         viewModel.shopList.observe(this) {
-            shopListAdapter.shopList = it
+            shopListAdapter.submitList(it)
         }
     }
 
@@ -74,7 +74,7 @@ class MainActivity : AppCompatActivity() {
 
             override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
                 val position = viewHolder.adapterPosition
-                val shopItem = shopListAdapter.shopList[position]
+                val shopItem = shopListAdapter.currentList[position]
                 viewModel.removeShopItem(shopItem)
             }
         })
