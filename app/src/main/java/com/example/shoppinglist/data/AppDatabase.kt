ba1222496707764844aa9d88eb77731f1f79d81b
Leonid Lavrov <274852@gmail.com>
2023-01-22 14:53:20 +0200

    Adding database to ShoppingList

---
+++ app/src/main/java/com/example/shoppinglist/data/AppDatabase.kt
@@ -0,0 +1,36 @@
+package com.example.shoppinglist.data
+
+import android.app.Application
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [ShopItemDbModel::class], version = 1, exportSchema = false)
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun shopListDao(): ShopListDao
+
+    companion object {
+        private const val DB_NAME = "shop_item.db"
+        private var db: AppDatabase? = null
+        private var lock = Any()
+
+        fun getInstance(application: Application): AppDatabase {
+            db?.let {
+                return it
+            }
+            synchronized(lock) {
+                db?.let {
+                    return it
+                }
+                val instance = Room.databaseBuilder(
+                    application,
+                    AppDatabase::class.java,
+                    DB_NAME
+                ).build()
+                db = instance
+                return instance
+            }
+        }
+    }
+}
